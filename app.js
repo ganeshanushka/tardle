d6ae4ee6a079ffb31cd9c87f5cf3c9a9090d0384
Anushka <anushkag@unc.edu>
2026-01-07 12:38:38 -0500

    Remove outdated date override, today's word (2026-01-07) is already FDOC

--- app.js
+++ app.js
@@ -512,12 +512,6 @@ let words = [
     // Create date string in YYYY-MM-DD format
     const dateString = `${year}-${month}-${day}`;
     
-    // Special override: Today's word (2026-01-05) is "FDOC"
-    // TODO: Remove this override after today to use the mapping below
-    if (dateString === '2026-01-05') {
-      return 'FDOC';
-    }
-    
     // Check if we have a word for this date in the mapping
     if (dailyWordMap[dateString]) {
       return dailyWordMap[dateString];
